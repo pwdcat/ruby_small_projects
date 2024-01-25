c7d91dc7cef129606e8987c2f1c16e5c38682d88
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-25 18:13:00 -0500

    knights_travalis: implemented tree stores "all" hits into results array outputs the last element, the element that stopped with the lowest depth

--- knights_travails/lib/tree.rb
+++ knights_travails/lib/tree.rb
@@ -1,26 +1,50 @@
 require_relative 'node'
 
 class Tree
-  attr_accessor :target
+  attr_accessor :target, :results
 
   def initialize
-    @target_depth = 6
+    @targetDepth = 6
   end
 
   MOVES = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]].freeze
 
   def knight_moves(inital, target)
     @target = target
+    @results = []
     build_tree(Node.new(inital))
   end
 
   def build_tree(node, depth = 0)
-    nil if depth == target_depth
+    return nil if depth > @targetDepth
+
+    if node.position == target
+      @targetDepth = depth
+      @results << node
+    end
 
     MOVES.each do |move|
-      posiiton = node.position.zip(move).map { |a, b| a + b }
-      child = Node.new(posiiton, node)
+      position = node.position.zip(move).map { |a, b| a + b }
+
+      next unless position.all? { |num| num.positive? && num < 8 }
+
+      child = Node.new(position, node)
       node.children << child
+      build_tree(child, depth + 1)
+    end
+    node
+  end
+
+  def output_moves
+    node = @results[-1]
+    p node.position
+    while node.parent
+      node = node.parent
+      p node.position
     end
   end
 end
+
+test = Tree.new
+test.knight_moves([0, 0], [3, 3])
+test.output_moves
